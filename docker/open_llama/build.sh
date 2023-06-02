3985e8f677a86455c217b78ad4ec525644418837
Gary Mulder <gjmulder@gmail.com>
2023-06-02 12:40:08 +0000

    Fixed model d/l path

--- docker/open_llama/build.sh
+++ docker/open_llama/build.sh
@@ -2,7 +2,7 @@
 
 MODEL="open_llama_3b"
 # Get  open_llama_3b_ggml q5_1 quantization
-python3 ./hug_model.py -a SlyEcho -s ${MODEL} -f "q5_1"
+python3 ./hug_model.py -a SlyEcho -s ${MODEL} -f open-llama-3b-q5_1.bin
 ls -lh *.bin
 
 # Build the default OpenBLAS image
